f1d589bfd66825269c0dec18028504e890a4b6da
Will Wayman <will.jdw@gmail.com>
2020-10-29 16:21:32 +0000

    Refactor

--- src/components/page/play.js
+++ src/components/page/play.js
@@ -1,96 +1,78 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Redirect } from "react-router-dom";
 
-import io from 'socket.io-client'
+import io from "socket.io-client";
 
-import AnswerForm from '../class/answer-form'
-import AskNextQuestionLink from '../function/ask-next-question-link'
-import EndCountdownButton from '../class/end-countdown-button'
-import GameContainer from '../container/game'
-import GuideLink from '../function/guide-link'
-import ImgEnlargedContainer from '../function/img-enlarged-container'
-import MenuTables from '../class/menu-tables'
-import PointsTable from '../function/points-table'
-import QuestionTables from '../function/question-tables'
+import AnswerForm from "../class/answer-form";
+import AskNextQuestionLink from "../function/ask-next-question-link";
+import EndCountdownButton from "../class/end-countdown-button";
+import GameContainer from "../container/game";
+import GuideLink from "../function/guide-link";
+import ImgEnlargedContainer from "../function/img-enlarged-container";
+import MenuTables from "../class/menu-tables";
+import PointsTable from "../function/points-table";
+import QuestionTables from "../function/question-tables";
 
-class Game extends Component {
-  constructor (props) {
-    super(props)
+const Game = ({ api, game, cookie, updateGame, handleClick }) => {
+  const _socket = game ? io(`${api}/${game.gamecode}`) : null;
+  const imgEnlarged = null;
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-
-    const { api, game } = this.props
-
-    this.state = {
-      _socket: game ? io(`${api}/${game.gamecode}`) : null,
-      imgEnlarged: null
-    }
-  }
-
-  componentDidMount () {
-    const { _socket } = this.state
-
+  useEffect(() => {
     if (_socket) {
-      _socket.on('remove player', id => {
-        const { cookie, updateGame } = this.props
-
+      _socket.on("remove player", (id) => {
         if (cookie === +id) {
-          updateGame()
+          updateGame();
         }
-      })
-      _socket.on('update game', game => this.props.updateGame(game))
+      });
+      _socket.on("update game", (game) => updateGame(game));
     }
-  }
+    return () => {
+      if (_socket) {
+        _socket.off("remove player");
+        _socket.off("update game");
+      }
+    };
+  });
 
-  componentWillUnmount () {
-    const { _socket } = this.state
+  const handleChange = (event) => {};
 
-    if (_socket) {
-      _socket.off('remove player')
-      _socket.off('update game')
-    }
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
-  handleChange (event) {
+  const _redirect = game ? (game.host.id ? null : "/game/host") : "/";
 
-  }
+  return (
+    <>
+      {_redirect ? (
+        <Redirect to={_redirect} />
+      ) : (
+        <>
+          <GameContainer game={game}>
+            <div className="sub-heading">
+              Asking the questions is... <span id="host">{game.host.name}</span>
+            </div>
+            {!game.counting && cookie === game.host.id && (
+              <AskNextQuestionLink />
+            )}
+            {!game.counting && <PointsTable players={game.players} />}
+            {!game.counting && cookie === game.host.id && (
+              <MenuTables api={api} game={game} />
+            )}
+            {game.counting && cookie === game.host.id && (
+              <EndCountdownButton api={api} game={game} />
+            )}
+            {game.counting && cookie !== game.host.id && (
+              <AnswerForm api={api} game={game} cookie={cookie} />
+            )}
+            <QuestionTables api={api} game={game} handleClick={handleClick} />
+            <GuideLink />
+          </GameContainer>
+          {imgEnlarged && <ImgEnlargedContainer />}
+        </>
+      )}
+    </>
+  );
+};
 
-  async handleSubmit (event) {
-    event.preventDefault()
-  }
-
-  render () {
-    const { cookie, game } = this.props
-    const { imgEnlarged } = this.state
-
-    const _redirect = game ? game.host.id ? null : '/game/host' : '/'
-
-    if (_redirect) {
-      return <Redirect to={_redirect} />
-    }
-
-    return (
-      <>
-        <GameContainer game={game}>
-          <div className='sub-heading'>Asking the questions is... <span id='host'>{game.host.name}</span></div>
-
-          {(!game.counting && cookie === game.host.id ? <AskNextQuestionLink /> : null)}
-          {(!game.counting ? <PointsTable players={game.players} /> : null)}
-          {(!game.counting && cookie === game.host.id ? <MenuTables {...this.props} /> : null)}
-
-          {(game.counting && cookie === game.host.id ? <EndCountdownButton {...this.props} /> : null)}
-          {(game.counting && cookie !== game.host.id ? <AnswerForm {...this.props} /> : null)}
-
-          <QuestionTables {...this.props} />
-
-          <GuideLink />
-        </GameContainer>
-
-        {(imgEnlarged ? <ImgEnlargedContainer /> : null)}
-      </>
-    )
-  }
-}
-
-export default Game
+export default Game;
